8439506ca911c767ad123f84544b7eabb970e265
allanalvarado94 <75002131+allanalvarado94@users.noreply.github.com>
2020-11-24 22:37:00 -0600

    Add files via upload
    this is my first start

---
+++ mIN.java
@@ -0,0 +1,43 @@
+ 
+import java.JOptionpane;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+//global variable 
+
+int nG;
+public class mIN
+
+
+{
+public static void main(String [] args ){
+Scan = new Scanner(System.in);
+
+int utilidad;
+double v1;
+
+int numx ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
